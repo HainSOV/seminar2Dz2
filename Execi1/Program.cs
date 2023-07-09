0582dd6b8fa7966fdcdab7775f02e317f91d7d7c
Mikail Sobolev <ya@hsov.ru>
2023-07-09 21:00:02 +0600

    решение задачи №10

--- Execi1/Program.cs
+++ Execi1/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите трехзначное число: ");
+int number = int.Parse(Console.ReadLine());
+if(number < 100 || number > 1000)
+{
+    Console.WriteLine("Вы вели не рехзначное число, попробуйте снова");
+    return;
+}
+Console.Write("вторая цифра трехзначного числа: " + number / 10 % 10);
