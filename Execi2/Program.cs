5519a2e5383864e7ab812e806328879dabb15296
Mikail Sobolev <ya@hsov.ru>
2023-07-09 22:36:27 +0600

    Решил задачу номер 13

--- Execi2/Program.cs
+++ Execi2/Program.cs
@@ -1,1 +1,25 @@
-﻿
+﻿int thirdRank(int number)
+{
+    while (number > 999)
+    {
+        number /= 10;
+    }
+    return number % 10;
+}
+
+bool notThreeDigit(int number)
+{
+    if (number < 100)
+    {
+        Console.WriteLine("Число не трехзначное");
+        return false;
+    }
+    return true;
+}
+
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+if (notThreeDigit(number))
+{
+    Console.WriteLine(thirdRank(number));
+}
