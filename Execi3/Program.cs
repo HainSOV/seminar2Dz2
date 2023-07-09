8dc8d8b3e1d7f3f1c5749c9ecd1190cc85f1ea9e
Mikail Sobolev <ya@hsov.ru>
2023-07-09 22:11:48 +0600

    Решение задачи номер 15

--- Execi3/Program.cs
+++ Execi3/Program.cs
@@ -2,3 +2,35 @@
 //6 -> да
 //7 -> да
 //1 -> нет
+bool weekdays(int day)
+{
+    if (day <= 5)
+    {
+        return true;
+    }
+    return false;
+}
+
+bool weektrue(int dayWeekTrue)
+{
+    if (dayWeekTrue > 0 && dayWeekTrue <= 7)
+    {
+        return true;
+    }
+Console.Write("Это не день недели");
+return false;
+}
+
+Console.Write("Какой по счету день недели: ");
+int Weekday = int.Parse(Console.ReadLine());
+if (weektrue(Weekday))
+{
+    if (weekdays(Weekday))
+    {
+        Console.Write("Еще рабочий день");
+    }
+    else
+    {
+        Console.Write("Ураа, выходной!!");
+    }
+}
